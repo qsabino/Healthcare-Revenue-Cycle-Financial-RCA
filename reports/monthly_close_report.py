ec17c22bc161c52d5631c52bff13562a720d657f
Quyen Di Sabino <153547241+qsabino@users.noreply.github.com>
2026-09-22 02:46:39 -0400

    Some minor changes on code

---
+++ reports/monthly_close_report.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from scripts.utils import load_sql
+
+def get_monthly_close_report(engine):
+    monthly_df = pd.read_sql(
+        load_sql("queries/finance/07_monthly_close.sql"),
+        engine
+    )
+    mom_df = pd.read_sql(
+        load_sql("queries/finance/08_mom_variance.sql"),
+        engine
+    )
+    return monthly_df, mom_df
