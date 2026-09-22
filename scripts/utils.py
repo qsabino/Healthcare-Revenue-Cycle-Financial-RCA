c8e016524af6b0755ee5335b6f30e2d5c28dda75
Quyen Di Sabino <153547241+qsabino@users.noreply.github.com>
2026-09-22 02:41:34 -0400

    Some minor changes on code

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,46 +1,23 @@
-
 from pathlib import Path
 
 def load_sql(path):
-
     with open(path, "r") as file:
         return file.read()
 
 
-
 # Basic Formatting for Reports
-
-def format_worksheet(
-    worksheet,
-    df
-):
-
+def format_worksheet(worksheet, df):
     worksheet.freeze_panes(1, 0)
-
-    worksheet.autofilter(
-        0,
-        0,
-        len(df),
-        len(df.columns)-1
-    )
-
-    worksheet.set_column(
-        0,
-        len(df.columns),
-        18
-    )
+    worksheet.autofilter(0, 0, len(df), len(df.columns)-1)
+    worksheet.set_column(0, len(df.columns), 18)
 
 
 # To protect database password
-
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 import os
-
 def get_engine():
-
     load_dotenv()
-
     connection_string = (
         f"postgresql://"
         f"{os.getenv('DB_USER')}:"
@@ -49,7 +26,4 @@ def get_engine():
         f"{os.getenv('DB_PORT')}/"
         f"{os.getenv('DB_NAME')}"
     )
-
-    return create_engine(
-        connection_string
-    )
+    return create_engine(connection_string)
