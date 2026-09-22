1290073cd9d2c472ae0e38e6143251b4d0dbe4cc
Quyen Di Sabino <153547241+qsabino@users.noreply.github.com>
2026-09-22 02:43:41 -0400

    Some minor changes on code

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 from scripts.clean_data import clean_data
-
 from scripts.load_to_postgres import load_to_postgres
-
 from build_workbook import build_workbook
 
 def main():
-
     clean_data()
-
     load_to_postgres()
-
     build_workbook()
-
     print("\nPipeline Complete")
 
 if __name__ == "__main__":
